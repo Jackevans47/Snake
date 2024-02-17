9a2a532327c0c3b86eee9c49c3ca13a62752d74d
jackevans47 <jack_evans47@outlook.com>
2024-02-17 13:03:46 +0000

    Add game area

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import curses
+from curses import textpad
+
 def next():
     print()
 
@@ -7,6 +10,14 @@ def food():
 def game_over():
     print()
 
-def main():
-    print()
+def main(screen):
+    curses.curs_set(0)
+    height,width = screen.getmaxyx()
+    box = [[2,2], [height-2, width-2]]
+    textpad.rectangle(screen, box[0][0], box[0][1], box[1][0], box[1][1])
+    screen.refresh()
+    screen.getch()
+
+
+curses.wrapper(main)
 
