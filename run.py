2fd9722a20a84e0e948dee1a82365689992ad6b5
jackevans47 <jack_evans47@outlook.com>
2024-02-17 10:23:45 +0000

    Add initial functions

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def next():
+    print()
+
+def food():
+    print()
+
+def game_over():
+    print()
+
+def main():
+    print()
+
