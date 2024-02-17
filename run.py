7668ae0f6e6f65f1af9207846e944a874d76569a
jackevans47 <jack_evans47@outlook.com>
2024-02-17 13:15:28 +0000

    Create snake and starting position

--- run.py
+++ run.py
@@ -11,11 +11,21 @@ def game_over():
     print()
 
 def main(screen):
+    """
+    Game area
+    """
     curses.curs_set(0)
     height,width = screen.getmaxyx()
     box = [[2,2], [height-2, width-2]]
     textpad.rectangle(screen, box[0][0], box[0][1], box[1][0], box[1][1])
-    screen.refresh()
+    
+
+    snake = [[height//2, width//2+1], [height//2, width//2], [height//2, width//2-1]]
+    direction = curses.KEY_RIGHT
+
+    for y,x in snake:
+        screen.addstr(y, x, "#")
+
     screen.getch()
 
 
